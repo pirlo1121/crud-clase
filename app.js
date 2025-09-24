0e8dfc8d211b4b90c420e08e8d5dffde89b5254e
Maicol <pirlo1121s@gmail.com>
2025-09-23 20:37:55 -0500

    create .env - .gitignore - -env-example

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import express from 'express'; // module = javascript
 import { connectDB } from './src/config/db.config.js';
 // const express = require('express');  commonJs = nodeJs
+import { configDotenv } from 'dotenv';
+configDotenv()
+
 
 const app = express();
 
 connectDB();
+const port = process.env.PORT
 
-app.listen(3000, ()=>{
-    console.log(`Servidor corriendo en el puerto 3000`)
+app.listen(port, ()=>{
+    // console.log(`Servidor corriendo en el puerto ${port}`)
+    console.log('Servidor corriendo en el puerto', port)
+
 });
--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import { configDotenv } from 'dotenv';
+configDotenv()
+
 
 export async function connectDB(){
     try {
-        await mongoose.connect('mongodb://localhost:27017')
+        await mongoose.connect(process.env.DB_URI)
         console.log('conectado a la base de datos')
     } catch (error) {
         console.log(error)
